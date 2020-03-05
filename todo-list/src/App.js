985fa0b42f0ef32994f8d7b526832b696a761e02
Keegan Nair <keegan.nair@hotmail.com>
2020-03-05 15:08:43 +1100

    adding uuid to get random ids/ can add todos but not saved anywhere yet

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList'
+import uuidv4 from 'uuid/v4' //generates random id
 
 function App() {
   const [todos, setTodos] = useState([])
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if(name === '') return
+    setTodos(prevTodos =>{
+      return [...prevTodos, {id: uuidv4(), name:name, completed: false}]
+    })
+    console.log(name)
+    todoNameRef.current.value = null; //after we type something and hit button the field will clear
+    
+  }
+
   return (
     <>
       <TodoList todos={todos}/>
-      <input type="text" />
-      <button>Add Todo </button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick = {handleAddTodo}>Add Todo </button>
       <button>Clear completed</button>
       <div>0 left to do </div>
     </>
