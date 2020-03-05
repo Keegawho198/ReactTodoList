1fcfecb96dd81a278cc49a3ce020e40da1bfb0ad
Keegan Nair <keegan.nair@hotmail.com>
2020-03-05 15:38:46 +1100

    saves todos when refreshed

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,16 +1,29 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
 import uuidv4 from 'uuid/v4' //generates random id
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef();
 
+  useEffect(() =>{
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos]) //when array of todos changes it will get saved through local storage
+  //using useeffect function
+
+
   function handleAddTodo(e){
     const name = todoNameRef.current.value
     if(name === '') return
     setTodos(prevTodos =>{
-      return [...prevTodos, {id: uuidv4(), name:name, completed: false}]
+      return [...prevTodos, {id: uuidv4(), name:name, complete: false}]
     })
     console.log(name)
     todoNameRef.current.value = null; //after we type something and hit button the field will clear
