60d34a1fd93908969cac3e14e90d3a37dcd9a0ed
Keegan Nair <keegan.nair@hotmail.com>
2020-03-05 14:39:39 +1100

    Added reacted files for tolist with some code for the buttons and input in app.js

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './TodoList'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <>
+    <TodoList/>
+    <input type = "text"/>
+    <button>Add Todo </button>
+    <button>Clear completed</button>
+    <div>0 left to do </div>
+    </>
+  )
 }
 
 export default App;
