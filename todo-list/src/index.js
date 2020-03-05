e4456a944a930d2104cb3f1ccaeb11f52e3d03b6
Keegan Nair <keegan.nair@hotmail.com>
2020-03-05 15:57:23 +1100

    Added some notes

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -3,3 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+//ref https://www.youtube.com/watch?v=hQAHSlTtcmY
+
+//webdev guide
+
+//also need to add some css to make it look better
