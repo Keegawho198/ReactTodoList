e01e6a8b48b2bbab66c893b0c13fb5514d5dbfdd
Keegan Nair <keegan.nair@hotmail.com>
2020-03-05 14:57:35 +1100

    adding checkbox using complete value

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import TodoList from './TodoList'
 
 function App() {
-  const [todos, setTodos] = useState(['todo1', 'todo2'])
+  const [todos, setTodos] = useState([])
   return (
     <>
       <TodoList todos={todos}/>
